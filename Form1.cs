096aceb8b5ea7b73552aa97324d6171397526910
Marc Raymond <59947352+mkr123456@users.noreply.github.com>
2020-05-31 17:54:06 -0700

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Data.SqlClient;
+using System.Collections.ObjectModel;
+
+
+
+using System.Windows.Forms;
+
+namespace matchingpairs6
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+        List<string> icons = new List<string>()
+        {
+            "!","!","N","N",",",",","k","k",
+            "b","b","v","v","w","w","z","z"
+        };
+        private object tableLayoutPane11;
+
+        public Form1()
+        {
+            InitializeComponent();
+            AssignIconsToSquares();
+
+
+        }
+
+        private void AssignIconsToSquares()
+        {
+
+            foreach (Control control in tableLayoutPane11.Controls)
+            {
+
+                Label iconLabel = control as Label;
+                if (iconLabel != null)
+                {
+                    int randomNumber = random.Next(icons.Count);
+                    iconLabel.Text = icons[randomNumber];
+
+                    iconLabel.ForeColor = iconLabel.BackColor;
+                    icons.RemoveAt(randomNumber);
+
+                }
+            }
+
+        }
+
+    }
+
+}
---
+++ MatchingPairs.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MatchingPairs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
